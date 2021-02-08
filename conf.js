7e9ae4a5adce22aee29e9a75ea298274d24dc3f1
Josh Sellers <josh.sellers@fox.com>
2021-02-08 00:40:38 -0800

    testing

--- conf.js
+++ conf.js
@@ -1,14 +1,13 @@
 // conf.js
 exports.config = {
-    framework: 'jasmine',
+    framework: 'mocha',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['spec.js'],
     onPrepare: function() {
         browser.ignoreSynchronization = true;
     },
     multiCapabilities: [{
-        browserName: 'firefox'
-    }, {
+        // browserName: 'firefox'}, {
         browserName: 'chrome'
     }]
 };
--- spec.js
+++ spec.js
@@ -8,30 +8,50 @@
 //});
 // spec.js
 // spec.js
+// adding a comment
 
-var expect = require('chai').expect;
+var chai = require('chai');
+chai.use(require('chai-smoothie'));
+var expect = chai.expect;
+var until = protractor.until;
+
 
 describe('Protractor Demo App', function() {
     var userName = element(by.id('username'));
     var password = element(by.id('password'));
     var submitBtn = element(by.id('submit'));
+    var logOut = element(by.linkText('.jslogoutLink.subNavList.link'));
+    var accountDropdown = element(by.css('.js-navSubMenuToggle'));
     // var latestResult = element(by.binding('latest'));
 
     beforeEach(function() {
         browser.get('https://turo.com/login');
     });
 
-    it('should have a title', function() {
-        expect(browser.getTitle()).toEqual('Turo');
-    });
+    //it('should have a title', function() {
+        // var title = browser.getTitle();
+        // expect(browser.getTitle()).to.include('Turo');
+     //   expect(element(by.css('h4'))).to.eventually.have.text('Welcome back')
+    //});
 
     it('should login', function() {
         userName.sendKeys('josh.sellers@turo.com');
         password.sendKeys('firerootlife');
 
         submitBtn.click();
-        //expect(browser.).to.have('')
+        //expect(browser.).to.include('')
 
         // expect(latestResult.getText()).toEqual('3');
     });
+
+    it('should logout', function () {
+        browser.ignoreSynchronization = true;
+        userName.sendKeys('josh.sellers@turo.com');
+        password.sendKeys('firerootlife');
+        submitBtn.click();
+        browser.wait(until.elementIsVisible(accountDropdown), 5000).then(function(accountDropdown) {
+            accountDropdown.click();
+        });
+        logOut.click();
+    })
 });
